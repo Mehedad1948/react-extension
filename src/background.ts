22b36da645faab2c340fb28a379261776c5f62c8
Mehrdad <mnourib13@gmail.com>
2025-10-17 19:27:04 +0330

    first commit

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,8 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "openPopup" && request.pageTitle) {
-    // Save page title
+    
     chrome.storage.local.set({ currentPageTitle: request.pageTitle }, () => {
-      // Open the popup
+      
       if (chrome.action.openPopup) {
         chrome.action.openPopup();
         sendResponse({ status: "Popup opened" });
@@ -10,6 +10,6 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         sendResponse({ status: "Popup could not be opened" });
       }
     });
-    return true; // keep message channel open
+    return true; 
   }
 });
